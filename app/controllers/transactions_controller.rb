94fead95232a38784ca3df9afc9e7cf854ed6595
vlanzillotta <vlanzillotta@gmail.com>
2014-05-13 13:58:26 -0400

    Transaction indexing for current user

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,6 +2,22 @@ class TransactionsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+		@current_user = current_user
+		@current_user_id = @current_user.id
+
+		
+
+
+		@transaction  = @current_user.transactions
+
+
+	end
+
+	def new
+		
+	end
+
+	def create
 		
 	end
 end
