4cd824c741dababdefc69cdef9553db45e52c80c
vlanzillotta <vlanzillotta@gmail.com>
2014-05-14 17:18:50 -0400

    Added capybara

--- Gemfile
+++ Gemfile
@@ -47,3 +47,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'capybara'
---
+++ spec/features/transactions_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+include Devise::TestHelpers
+
+describe "Transactions" , :type => :feature do
+  
+  describe "View transactions" do
+    
+    it "When not logged in" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/transactions'
+      expect(page).to have_content('Sign in')
+    end
+
+	let(:user) {User.find(1)}
+    
+    it "when logged in" do
+    	# sign_in user
+    	# visit '/transactions'
+     #    expect(page).to have_content('Here are your transactions!')
+    end
+
+
+
+  end
+end
