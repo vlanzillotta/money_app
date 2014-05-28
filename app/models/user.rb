2f5706841cbd43e33d33ff2c47b923a64e885019
vlanzillotta <vlanzillotta@gmail.com>
2014-05-27 22:13:58 -0400

    Added balance method to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :transactions
+
+  def balance
+  	transactions.sum("amount").to_i
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe User do
+	before do
+	    	@user = User.create(email: "vlanzillotta@gmail.com", 
+	    		password: "password", 
+	    		password_confirmation: "password")
+
+    		populate_transactions @user
+  	end
+  	subject { @user }
+
+	it {should respond_to(:balance)}
+
+	describe "balance results" do
+		its(:balance) { should eq 2300.00 } 
+	end
+
 end
