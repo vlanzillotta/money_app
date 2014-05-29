5438cb6fcae96c1e2de49d361e14ed103a5f7a8c
vlanzillotta <vlanzillotta@gmail.com>
2014-05-29 16:58:16 -0400

    Added signin fields to the sign in page and refactored the test cases to use IDs so as not to get confused between the two forms.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -16,9 +16,9 @@ end
 
 def sign_in (user)
 	visit new_user_session_path
-	fill_in "user_email",    with: user.email
-    fill_in "user_password", with: user.password
-    click_button "Sign in"
+	fill_in "main_user_email",    with: user.email
+    fill_in "main_user_password", with: user.password
+    click_button "Signin"
 end
 
 
