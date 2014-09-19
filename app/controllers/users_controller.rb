204a24bff00a7075eb03284b61414a643bc4f667
vlanzillotta <vlanzillotta@gmail.com>
2014-09-19 15:56:48 -0400

    Updated view to use next transactions instead of future transactions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 	def dashboard
 		@recent_transactions  = current_user.transactions.where(type_of: ["credit", "expense"])
 		@future_transactions  = current_user.transactions.where type_of: "future transaction" 
+		@next_transactions  = current_user.next_transactions
 		@putaway_transactions  = current_user.transactions.where type_of: "putaway transaction"
 		@user_balance = balance;
 		@user_future_balance = future_balance;
