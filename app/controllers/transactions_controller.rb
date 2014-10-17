b7823e402af15d1168e845e315aed8a53a6edc1a
vlanzillotta <vlanzillotta@gmail.com>
2014-10-17 15:38:17 -0400

    User can no longer make/edit a repeating transaction without a transaction date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -47,6 +47,8 @@ class TransactionsController < ApplicationController
 		@current_user = current_user
 		saved_transaction = @current_user.transactions.create(transaction_params)
 
+
+		
 		create_repeating_transaction_instances saved_transaction
 
 		redirect_to transactions_path
@@ -88,6 +90,13 @@ class TransactionsController < ApplicationController
 		end
 
 		def create_repeating_transaction_instances(transaction)
+
+			if(transaction.repeat_frequency && !transaction.transaction_date )	
+				transaction.repeat_frequency = ""
+				transaction.save
+				flash[:notice] = "You cannot have a repeat frequency without a starting date"
+			end
+
 			case transaction.repeat_frequency
 				when "daily"
 					(1..32).each do |i|
